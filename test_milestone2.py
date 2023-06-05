945da6b88ca645712bdc3660662a55122492d679
Justin Peeples <97637495+jpeeps2400@users.noreply.github.com>
2023-06-05 16:35:20 -0600

    test_store function uploaded

--- test_milestone2.py
+++ test_milestone2.py
@@ -9,4 +9,13 @@ def test_load():
     main.load(6)
     assert main.acc == "1006"
 
+def test_store():
+    global acc
+    main.acc = "+1345"
+
+    global mem
+    main.mem = ['1000','1001','1002','1003','1004','1005','1006','1007','1008','1009','1010','1011']    #initializes the 
+    main.store(6)
+    assert main.mem[6] == "+1345"
+
 test_load()
