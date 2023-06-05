23dd4d20a09098d028925c20d8396c93f50c316e
Justin Peeples <97637495+jpeeps2400@users.noreply.github.com>
2023-06-05 16:27:39 -0600

    Test file working locally

--- test_milestone2.py
+++ test_milestone2.py
@@ -1,7 +1,12 @@
 import pytest
 import main
 def test_load():
-    main.load(21)
-    assert main.acc == 21
+    global acc
+    main.acc = "+0000"
+
+    global mem
+    main.mem = ['1000','1001','1002','1003','1004','1005','1006','1007','1008','1009','1010','1011']    #initializes the 
+    main.load(6)
+    assert main.acc == "1006"
 
 test_load()
