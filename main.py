bcfa6e7297e9ce54bcd3a56537d9db747c8ec97d
Justin Peeples <97637495+jpeeps2400@users.noreply.github.com>
2023-06-05 15:38:22 -0600

    Update main.py

--- main.py
+++ main.py
@@ -40,55 +40,53 @@ acc = 0     #initializes the accumulator
 mem = []    #initializes the memory
 ip = 0      #initializes the intruction pointer
 line = 1
-def main():
-    file = input("What is your file name?: ")   #gets file name from user
-    f = open(file, "r", encoding="utf8")        #opens file
-    for x in f:
-        if x in ("-99999"):
-            f.close()
-            break           #if word is -99999, stop reading and close the file
+file = input("What is your file name?: ")   #gets file name from user
+f = open(file, "r", encoding="utf8")        #opens file
+for x in f:
+    if x in ("-99999"):
+        f.close()
+        break           #if word is -99999, stop reading and close the file
 
-        word = x[0:-1]      #set word to one smaller than word length to remove newline
-        if len(word) != 5:   #if word is longer than 5 chars throw an error
-            raise ValueError("incorrect input format on line " + str(line))
+    word = x[0:-1]      #set word to one smaller than word length to remove newline
+    if len(word) != 5:   #if word is longer than 5 chars throw an error
+        raise ValueError("incorrect input format on line " + str(line))
 
-        if (word[0] != '+') and (word[0] != '-'):   #if word doesn't start with + or - throw an error
-            raise ValueError("incorrect input format on line " + str(line))
+    if (word[0] != '+') and (word[0] != '-'):   #if word doesn't start with + or - throw an error
+        raise ValueError("incorrect input format on line " + str(line))
 
-        mem.append(word)    #append the word to memory
-        #print(mem[-1])
-        line += 1
-    f.close()
-    while ip < len(mem):            #runs the code
-        curr_word = mem[ip]         #set curret word = to the word the IP is pointing to
-        if curr_word[0] == "-":     #if the first char is '-' then go to the next word
-            ip += 1
-            continue
-        op_code = curr_word[1:3]    #split op code and memory address into 2 variables
-        mem_add = int(curr_word[3:])
-        #print(op_code)
-        #print(mem_add)
-        if op_code == "10":     #read from keyboard into memory
-            read(mem_add)
-        elif op_code == "11":   #write from memory to screen
-            write(mem_add)
-        elif op_code == "20":   #Load a word from a specific location in memory into the accumulator.
-            load(mem_add)
-        elif op_code == "21":   #Store a word from the accumulator into a specific location in memory.
-            store(mem_add)
-        elif op_code == "40":   #branch to location in memory
+    mem.append(word)    #append the word to memory
+    #print(mem[-1])
+    line += 1
+f.close()
+while ip < len(mem):            #runs the code
+    curr_word = mem[ip]         #set curret word = to the word the IP is pointing to
+    if curr_word[0] == "-":     #if the first char is '-' then go to the next word
+        ip += 1
+        continue
+    op_code = curr_word[1:3]    #split op code and memory address into 2 variables
+    mem_add = int(curr_word[3:])
+    #print(op_code)
+    #print(mem_add)
+    if op_code == "10":     #read from keyboard into memory
+        read(mem_add)
+    elif op_code == "11":   #write from memory to screen
+        write(mem_add)
+    elif op_code == "20":   #Load a word from a specific location in memory into the accumulator.
+        load(mem_add)
+    elif op_code == "21":   #Store a word from the accumulator into a specific location in memory.
+        store(mem_add)
+    elif op_code == "40":   #branch to location in memory
+        branch(mem_add)
+        continue
+    elif op_code == "41":   #branch if accumulator is negative
+        if curr_word[0] == "-":
             branch(mem_add)
             continue
-        elif op_code == "41":   #branch if accumulator is negative
-            if curr_word[0] == "-":
-                branch(mem_add)
-                continue
-        elif op_code == "42":   #branch if the accumulator is zero
-            if int(curr_word[1:]) == 0:
-                branch(mem_add)
-                continue
-        elif op_code == "43":   #pause the program
-            break
+    elif op_code == "42":   #branch if the accumulator is zero
+        if int(curr_word[1:]) == 0:
+            branch(mem_add)
+            continue
+    elif op_code == "43":   #pause the program
+        break
 
-        ip += 1     #go to the next word
-main()
+    ip += 1     #go to the next word
