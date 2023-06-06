e547df5f3d5269eddc94dcfbe74bb46cae21fe85
Justin Peeples <97637495+jpeeps2400@users.noreply.github.com>
2023-06-05 18:30:05 -0600

    Updated test file

--- test_milestone2.py
+++ test_milestone2.py
@@ -1,21 +1,40 @@
 import pytest
 import main
-def test_load():
+#You will also create a spreadsheet that lists the unit tests in row/column form.  
+#Give each unit test a name, short description, the reference to the appropriate use case, inputs, 
+#expected outputs, and how you know whether the test succeeded or failed.
+
+def test_load_pass():
+    main.acc = "+0000"
+    main.mem = ['1000','1001','1002','1003','1004','1005','1006','1007','1008','1009','1010','1011']   
+    main.load(6)
+    assert main.acc == "1006"
+
+def test_load_fail():
     global acc
     main.acc = "+0000"
 
     global mem
-    main.mem = ['1000','1001','1002','1003','1004','1005','1006','1007','1008','1009','1010','1011']    #initializes the 
-    main.load(6)
-    assert main.acc == "1006"
+    main.mem = ['1000','1001','1002','1003','1004','1005','1006','1007','1008','1009','1010','1011']
+    with pytest.raises(IndexError):
+        main.load(13)
+    assert main.acc == "+0000"
 
-def test_store():
+def test_store_pass():
     global acc
     main.acc = "+1345"
 
     global mem
-    main.mem = ['1000','1001','1002','1003','1004','1005','1006','1007','1008','1009','1010','1011']    #initializes the 
+    main.mem = ['1000','1001','1002','1003','1004','1005','1006','1007','1008','1009','1010','1011']
     main.store(6)
     assert main.mem[6] == "+1345"
 
-test_load()
+def test_store_fail():
+    global acc
+    main.acc = "+1345"
+
+    global mem
+    main.mem = ['1000','1001','1002','1003','1004','1005','1006','1007','1008','1009','1010','1011']
+    with pytest.raises(IndexError):
+        main.store(-1)
+    assert main.mem[6] == "1006"
