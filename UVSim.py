52ef75bc84a6cb22601f95320d019fbf75120ab6
Justin Peeples <97637495+jpeeps2400@users.noreply.github.com>
2023-06-13 18:27:49 -0600

    Update UVSim.py

--- UVSim.py
+++ UVSim.py
@@ -20,9 +20,9 @@ class UVSim:
         break           #if word is -99999, stop reading and close the file
       word = x[0:-1]      #set word to one smaller than word length to remove newline
       if len(word) != 5:   #if word is longer than 5 chars throw an error
-        raise ValueError("incorrect input format on line " + str(line))
+        raise ValueError("incorrect input format on line " + str(self.line))
       if (word[0] != '+') and (word[0] != '-'):   #if word doesn't start with + or - throw an error
-        raise ValueError("incorrect input format on line " + str(line))
+        raise ValueError("incorrect input format on line " + str(self.line))
       self.mem[self.line - 1] = word     #add the word to memory at line minus 1
       self.line += 1
     f.close()
