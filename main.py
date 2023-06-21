a748538e51718d8debf465d85b79d9ddbeb90c11
MistaMaciii <macsnow66@gmail.com>
2023-06-21 11:32:33 -0600

    deletions of legacy code

--- main.py
+++ main.py
@@ -1,38 +1,3 @@
-"""
-UVSim - Simple Virtual Machine 
-Elena Mitchell, Justin Peeples, Mac Snow, Wes James
-Utah Valley University
-"""
-# import UVSim
-# from PyQt5.QtWidgets import *
-# from PyQt5.QtCore import QThread, pyqtSignal
-
-# class UVSimThread(QThread):
-#     finished = pyqtSignal()
-
-#def run(self):
-#     uvSim = UVSim.UVSim()
-#     uvSim.loadFile()
-#     uvSim.runSystem()
-#     self.finished.emit()
-
-# def main():
-#     app = QApplication([])
-#     window = QWidget()
-#     label = QLabel("UVSim")
-#     button = QPushButton("Run Program")
-#     layout = QVBoxLayout()
-#     layout.addWidget(label)
-#     layout.addWidget(button)
-#     window.setLayout(layout)
-#     window.show()
-
-#     thread = UVSimThread()
-#     thread.finished.connect(app.quit)
-
-#     button.clicked.connect(thread.start)
-
-#     app.exec_()
 import UVSim
 import sys
 from PyQt6.QtWidgets import QMainWindow, QApplication, QLabel, QToolBar, QStatusBar, QFileDialog
