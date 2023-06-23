ff04ddf74ba32f46ec5af46b7aee99797bf1a7b2
MistaMaciii <macsnow66@gmail.com>
2023-06-23 12:48:39 -0600

    Small changes

--- main.py
+++ main.py
@@ -1,47 +1,59 @@
 import UVSim
 import sys
-from PyQt6.QtWidgets import QMainWindow, QApplication, QLabel, QToolBar, QStatusBar, QFileDialog
-from PyQt6.QtGui import QAction, QIcon
-from PyQt6.QtCore import Qt
+import io
+from PyQt6.QtWidgets import QMainWindow, QApplication, QLabel, QToolBar, QStatusBar, QFileDialog, QPlainTextEdit, QMessageBox, QLineEdit, QVBoxLayout, QWidget
+from PyQt6.QtGui import QAction
+from PyQt6.QtCore import Qt, pyqtSignal, QObject, QMetaObject
+from PyQt6.QtGui import QTextCursor
 
 class MainWindow(QMainWindow):
-
     def __init__(self):
         super(MainWindow, self).__init__()
-        
-        #initalize UVSim
+
+        # Initialize UVSim
         self.uvSim = UVSim.UVSim()
 
-        #initialize file_path
+        # Initialize file_path
         self.file_path = False
 
-        #Set main window
+        # Main Layout
+        main_widget = QWidget()
+        main_layout = QVBoxLayout()
+        self.setCentralWidget(main_widget)
+        main_widget.setLayout(main_layout)
+
+        # Set main window
         self.setWindowTitle("UVSim")
         label = QLabel("")
         label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.setFixedSize(800, 450)
 
-        self.setCentralWidget(label)
-
-        #Set toolbar
+        # Set toolbar
         toolbar = QToolBar("Menu")
         self.addToolBar(toolbar)
-        
-        #Add the 'file' selector button to the toolbar
+        toolbar.setMovable(False)
+
+        # Add the 'file' selector button to the toolbar
         self.button_file = QAction("File...")
-        self.button_file.setStatusTip("Select the file the run")
+        self.button_file.setStatusTip("Select the file to run")
         self.button_file.triggered.connect(self.onToolBarFileButtonClick)
         toolbar.addAction(self.button_file)
 
-        #Add the 'run' button to the toolbar
+        # Add the 'run' button to the toolbar
         self.button_run_program = QAction("Run", self)
         self.button_run_program.setStatusTip("Run the file through UVSim")
         self.button_run_program.triggered.connect(self.onToolBarRunButtonClick)
         toolbar.addAction(self.button_run_program)
 
-        #Button 'Checker'
+        # Button 'Checker'
         self.button_is_checked = False
 
         self.setStatusBar(QStatusBar(self))
+
+        #Add Console Output
+        console_output = QLineEdit()
+        console_output.setReadOnly(True)
+        main_layout.addWidget(console_output)
 
     def onToolBarFileButtonClick(self):
         if self.button_is_checked == False:
@@ -53,24 +65,22 @@ class MainWindow(QMainWindow):
                 filter="All Files (*)")
             self.uvSim.loadFile(self.file_path)
             
-            self.button_is_checked = False     #reset button after system is finished
+            self.button_is_checked = False  # reset button after system is finished
     
     def onToolBarRunButtonClick(self):
-        if self.file_path == False:             #check if file_path exists
+        if self.file_path == False:  # check if file_path exists
             print("No file selected. Please select a file")
         else:
             if self.button_is_checked == False:
                 self.button_is_checked = True
                 self.uvSim.runSystem()
-                self.button_is_checked = False     #reset button after system is finished
+                self.button_is_checked = False  # reset button after system is finished
 
 def main():
-        
-    app = QApplication(sys.argv) #Open the QApp through MainWindow class with
-                                 # command line options
-    w = MainWindow()    
-    w.show()            #show main window          
-    app.exec()          #start event loop
+    app = QApplication(sys.argv)  # Open the QApp through MainWindow class with command line options
+    w = MainWindow()
+    w.show()  # show main window
+    sys.exit(app.exec())  # start event loop
 
 if __name__ == '__main__':
     main()
