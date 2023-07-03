60f7efb27a3220eb796d1c82c1b222581c3db3b7
Wes James <133700495+Wes-27@users.noreply.github.com>
2023-07-03 10:31:56 -0600

    Update UVSim.py

--- UVSim.py
+++ UVSim.py
@@ -13,25 +13,6 @@ class UVSim:
     self.line = 1
     self.memory = Memory.Memory()
 
-  def loadFile(self, path):
-    file = path                                 #grab file from path: pyqt6 widget --> windows dialog box
-    try:
-      with open(file, "r", encoding="utf8") as f:       #opens file
-        for x in f:
-          if x in ("-99999"):
-            f.close()
-            break                 #if word is -99999, stop reading and close the file
-          word = x[0:-1]          #set word to one smaller than word length to remove newline
-          if len(word) != 5:      #if word is longer than 5 chars throw an error
-            raise ValueError("incorrect input format on line " + str(self.line))
-          if (word[0] != '+') and (word[0] != '-'):   #if word doesn't start with + or - throw an error
-            raise ValueError("incorrect input format on line " + str(self.line))
-          self.memory.mem[self.line - 1] = word     #add the word to memory at line minus 1
-          self.line += 1
-    except FileNotFoundError:
-      self.output += "No file selected. Please select a file\n"
-      
-
   def runSystem(self, user_input):
     self.ip = 0             #if the system resets, re initialize to 0
     self.halt_status = False
