3509ed800d9939c58e06186a7232723ea7fb6d9d
Wes James <133700495+Wes-27@users.noreply.github.com>
2023-07-03 12:57:30 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ from PyQt6.QtWidgets import QApplication
 
 def main():
     app = QApplication(sys.argv)  # Open the QApp through MainWindow class with command line options
+    """
+    with open("style.css", "r") as file:
+        app.setStyleSheet(file.read())
+    """
     w = gui.MainWindow()
     w.show()  # show main window
     sys.exit(app.exec())  # start event loop
