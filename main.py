ca6f96ce29dc004cd532501e26db0f32937d9a67
Wes James <133700495+Wes-27@users.noreply.github.com>
2023-07-03 13:03:52 -0700

    Update main.py to include stylesheet link

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from PyQt6.QtWidgets import QApplication
 
 def main():
     app = QApplication(sys.argv)  # Open the QApp through MainWindow class with command line options
-    """
+   
     with open("style.css", "r") as file:    #opens file for css styles
         app.setStyleSheet(file.read())
-    """
+    
     w = gui.MainWindow()
     w.show()  # show main window
     sys.exit(app.exec())  # start event loop
