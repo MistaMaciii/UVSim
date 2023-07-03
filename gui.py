8eb6a4421bac9a9450b118ef7dee634bfc10d0f5
Wes James <133700495+Wes-27@users.noreply.github.com>
2023-07-03 12:46:28 -0600

    Update gui.py

--- gui.py
+++ gui.py
@@ -96,7 +96,7 @@ class MainWindow(QMainWindow):
                 filter="All Files (*)")
             self.loader.load_file(self.file_path)
             self.memory_textedit.clear()
-            mem = self.uvSim.memory.mem  # prints memory after loading file
+            mem = self.loader.mem  # prints memory after loading file
             self.update_memory_display(mem)
             self.updateConsoleDisplay()
             self.button_is_checked = False  # reset button after system is finished
