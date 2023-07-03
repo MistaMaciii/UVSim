8d4cd27140cfe9fba2cee09e18a8447920166c78
Justin Peeples <97637495+jpeeps2400@users.noreply.github.com>
2023-07-02 23:33:49 -0600

    UVSim added back to IO

--- Operations/IO_Operations.py
+++ Operations/IO_Operations.py
@@ -1,32 +1,33 @@
 class IO_Operations:
-  def pickOperation(operation, memLoc, UVSim, user_input):
+  def pickOperation(operation, memLoc, Memory, user_input, UVSim):
     if operation == "0":
-      IO_Operations.read(memLoc, UVSim, user_input)
+      IO_Operations.read(memLoc, Memory, user_input, UVSim)
     elif operation == "1":
-      IO_Operations.write(memLoc, UVSim)
+      IO_Operations.write(memLoc, Memory, UVSim)
       
-  def read(add, UVSim, user_input):
+  def read(add, Memory, user_input, UVSim):
     """Read a word from the keyboard into a specific location in memory"""
-    if add > len(UVSim.mem) -1:
+    if add > len(Memory.mem) -1:
         raise IndexError("Can't access memory at index " + str(add))
     UVSim.output += ("Input a word(+1234): \n")
     user_in =  user_input
 
     if (len(user_in) == 4 and user_in.isdigit()):   #if word is not == 5 chars redo
-        UVSim.mem[add] = str(user_in)
+        Memory.mem[add] = str(user_in)
     elif (len(user_in) == 5 and (user_in[0] == '-' or (user_in[0] == '+')) and user_in[1:].isdigit):
-        UVSim.mem[add] = str(user_in)
+        Memory.mem[add] = str(user_in)
     else:
+        
         UVSim.output += ("incorrect length\n")    
         print("incorrect length")
-        IO_Operations.read(add, UVSim)   #TypeError: IO_Operations.read() missing 1 required positional argument: 'UVSim' when incorrect length
+        IO_Operations.read(add, Memory)   #TypeError: IO_Operations.read() missing 1 required positional argument: 'Memory' when incorrect length
         return
   
-  def write(add, UVSim):
+  def write(add, Memory, UVSim):
     """Write a word from a specific location in memory to screen"""
-    if add > len(UVSim.mem) -1:
+    if add > len(Memory.mem) -1:
         raise IndexError("Can't access memory at index " + str(add))
-    out = UVSim.mem[add]
+    out = Memory.mem[add]
     UVSim.output += (str(out) + "\n")
     print(out)
     
